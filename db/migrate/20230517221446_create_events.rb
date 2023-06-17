cb40849828ead0f2a2a8350304c5113b3fa1373f
Blanca Rodriguez <blancarmp95@gmail.com>
2023-06-17 16:17:04 -0600

    add event model

--- db/migrate/20230517221446_create_events.rb
+++ db/migrate/20230517221446_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
     create_table :events do |t|
       t.string :name
       t.date :date
-      t.string :code
+      t.string :event_code
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

be615ab11124e8a14b707a0b6c5b7c7713c9d71b
Blanca Rodriguez <blancarmp95@gmail.com>
2023-06-17 16:11:59 -0600

    add models

--- db/migrate/20230617215233_add_phone_to_user.rb
+++ db/migrate/20230617215233_add_phone_to_user.rb
@@ -1,5 +1,5 @@
 class AddPhoneToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :phone, :integer
+    add_column :users, :phone, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_215233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "code"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "gifts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_gifts_on_event_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "gift_id", null: false
+    t.string "payment_method"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["gift_id"], name: "index_purchases_on_gift_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "events", "users"
+  add_foreign_key "gifts", "events"
+  add_foreign_key "purchases", "gifts"
+  add_foreign_key "purchases", "users"
 end

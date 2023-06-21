e278336730ab7863a9b3aefa37bf4d8d85c95363
Adrian Perez Juarez <adrianperezj@gmail.com>
2023-06-20 20:39:32 -0600

    gifts index

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,4 +2,9 @@ class Gift < ApplicationRecord
   belongs_to :event
   has_one :purchase, dependent: :destroy
   has_one :user, through: :purchase
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :event_id, presence: true
 end
